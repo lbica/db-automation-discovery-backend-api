5318addbb2c47d001e0d457fc97164918558c059
Laurentiu Bica <laurentiubica@yahoo.com>
2025-11-15 15:14:51 +0100

    Change the configuration package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Db Automation Discovery Backend API",
+    name="dbad-backend-api",
     version="0.1.0",
     packages=find_packages(where="app"),
     package_dir={"": "app"},
